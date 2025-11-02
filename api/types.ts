ec5fee844548662b90e638f352f6b558bbcf63bd
ekayab-ops <262583022+ekayab-ops@users.noreply.github.com>
2025-11-02 16:41:38 +0000

    Clean up UI layout as per new requirements

--- api/types.ts
+++ api/types.ts
@@ -3,3 +3,9 @@ type RecursivePartial<T> = {
   [P in keyof T]?: T[P] extends (infer U)[] ? RecursivePartial<U>[] : T[P] extends object ? RecursivePartial<T[P]> : T[P];
 };
 
+
+const validateEmail = (email: string): boolean => {
+  const re = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+  return re.test(email);
+};
+
